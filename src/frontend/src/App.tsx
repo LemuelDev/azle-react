d780aa1e6f253bab93efa87c95a34fffba8ada9d
John Lemuel Encina <lemueldev2023@gmail.com>
2024-10-15 02:34:20 +0000

    fixing dfx issue

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import AppRoutes from "./routes";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./section/Homepage";
 import Volunteerpage from "./section/Volunteerpage";
@@ -8,14 +7,16 @@ import AdminDashboard from "./section/AdminDashboard";
 function App() {
   return (
     <main className="w-auto h-auto">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/volunteer" element={<Volunteerpage />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin/*" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+      {/* Using HashRouter to ensure client-side routing works */}
+      <Router>
+        <Routes>
+          {/* Define routes with their respective components */}
+          <Route path="/" element={<Homepage />} />
+          <Route path="/volunteer" element={<Volunteerpage />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/admin/*" element={<AdminDashboard />} />
+        </Routes>
+      </Router>
     </main>
   );
 }
